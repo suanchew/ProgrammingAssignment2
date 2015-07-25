206dfa0c33dddc89aa91aaa315346203175a638f
suanchew <suanschew@yahoo.com>
2015-07-24 18:24:54 -0700

    Finished assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## The following functions computes the inverse of a matrix and then caches it. This is beneficial since matrix inversion is compute intensive. So, whenever an inverse is required and is available in the cache, the cached inverse is returned. The R function - solve - is used to compute the inverse. 
+
+
+## The function makeCacheMatrix() returns a "matrix" object that is essentially a list of functions: set(), get(), setinverse(), getinverse(). The set() and setinverse() functions use the <<- operator to cache input data and inversed results as matrix inversions are being computed. This allows the most recently computed inverse to be retrieved from the cache, thereby, achieving a "least burdensome" matrix inversion.
+
 
 makeCacheMatrix <- function(x = matrix()) {
+	
+	m <- NULL
+	
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	
+	get <- function() x
+	
+	setinverse <- function(solve) m <<- solve
+	
+	getinverse <- function() m
+	
+	list(set=set, get=get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
-
-## Write a short comment describing this function
+## The function cacheSolve() retrieves the cached inverse if it is available (non null) and returns it. Otherwise, it retrieves the cached input data and computes the inverse using the R function - solve. Whenever an inverse is computed, it is cached. This allows the most recently computed inverse to be retrieved from the cache, thereby achieving a "least burdensome" matrix inversion.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+	m <- x$getinverse()
+	
+	if (!is.null(m)){
+		message ("getting cached data")
+		return (m)
+	}
+
+	data <- x$get()
+	
+	m <- solve(data,...)
+	
+	x$setinverse(m)
+	
+	m
+
 }
+
+
+
+
